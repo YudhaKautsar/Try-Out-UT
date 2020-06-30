fb5337df512b00dff21ebe076f8a95814c08469b
YudhaKautsar <52621462+YudhaKautsar@users.noreply.github.com>
2020-06-30 15:41:59 +0700

    Add files via upload

---
+++ 001.js
@@ -0,0 +1,16 @@
+        let nilai = prompt('Inputkan nilai akhir:');
+        let grade = '';
+
+        if(nilai >= 90){
+            grade = 'A'
+        }else if(nilai >= 80){ 
+            grade = 'B'
+        }else if(nilai >= 70){
+            grade = 'C'
+        }else if(nilai >= 60){
+            grade = 'D'
+        }else {
+            nilai <= 59
+            grade = 'E';
+        }
+        document.write(`Grade anda: ${grade}`)
